1f49df90eacebf60a7aa703c85ca7d54fe6a21bc
daancarstens <55231123+daancarstens@users.noreply.github.com>
2020-06-17 12:28:15 +0200

    Add files via upload

---
+++ DaanThema/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <small>
+        &copy; <?php the_author(); ?>
+        <?php echo date('Y'); ?>
+    </small>
+</footer>
+</body> 
+</html>
---
+++ DaanThema/index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<?php  
+ if( have_posts() ) :
+    while( have_posts()) : the_post(); ?>
+       <h3> <a href="<?php the_permalink() ?>"><?php the_title(); ?></a> </h3>
+       <div> <?php the_excerpt(); ?> </div>
+
+
+<?php  
+   endwhile;
+else:  ?>
+   <p>geen bericht gevonden</p>
+<?php endif ?>
+<?php get_footer(); ?>
+
