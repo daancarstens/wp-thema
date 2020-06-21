838c0dc7ad39bf3387aaf09ee3ee57347b9922a3
daancarstens <55231123+daancarstens@users.noreply.github.com>
2020-06-21 17:35:23 +0200

    Add files via upload

--- DaanThema/index.php
+++ DaanThema/index.php
@@ -1,15 +1,30 @@
 <?php get_header(); ?>
-<?php  
- if( have_posts() ) :
-    while( have_posts()) : the_post(); ?>
-       <h3> <a href="<?php the_permalink() ?>"><?php the_title(); ?></a> </h3>
-       <div> <?php the_excerpt(); ?> </div>
+
+<div class="container">
+
+   <div class="row">
+      <main class="col-md-8">
+
+         <?php  
+          if( have_posts() ) :
+             while( have_posts()) : the_post(); ?>
+         <h3> <a href="<?php the_permalink() ?>"><?php the_title(); ?></a> </h3>
+         <div> <?php the_excerpt(); ?> </div>
 
 
-<?php  
-   endwhile;
-else:  ?>
-   <p>geen bericht gevonden</p>
-<?php endif ?>
+         <?php  
+            endwhile;
+         else:  ?>
+         <p>geen bericht gevonden</p>
+         <?php endif ?>
+      </main>
+
+      <aside class="col-md-4 bg-light">
+         <h3>
+            Terzijde
+         </h3>
+      </aside>
+   </div>
+</div>
+
 <?php get_footer(); ?>
-
